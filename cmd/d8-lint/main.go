50839fa66559a8fc8b8e022a8041431954676228
Evsyukov Denis <denis.evsyukov@flant.com>
2024-09-24 17:59:07 +0300

    wip
    Signed-off-by: Evsyukov Denis <denis.evsyukov@flant.com>

---
+++ cmd/d8-lint/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/deckhouse/d8-lint/pkg/config"
+	"github.com/deckhouse/d8-lint/pkg/flags"
+	"github.com/deckhouse/d8-lint/pkg/logger"
+	"github.com/deckhouse/d8-lint/pkg/manager"
+)
+
+func main() {
+	dirs := flags.ParseFlags()
+	if len(dirs) == 0 {
+		return
+	}
+
+	logger.InitLogger()
+	logger.InfoF("Dirs: %v", dirs)
+
+	cfg, err := config.NewDefault(dirs)
+	logger.CheckErr(err)
+
+	mng := manager.NewManager(dirs, cfg)
+	result := mng.Run()
+	if result.ConvertToError() != nil {
+		fmt.Printf("%s\n", result.ConvertToError())
+		os.Exit(1)
+	}
+}
