ba4d266fc6e99d02d8c3ee583dc4468c135e9e0a
Evsyukov Denis <denis.evsyukov@flant.com>
2024-11-13 19:23:49 +0300

    wip

--- cmd/dmt/main.go
+++ cmd/dmt/main.go
@@ -4,54 +4,22 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/spf13/pflag"
-
 	"github.com/deckhouse/dmt/internal/flags"
 	"github.com/deckhouse/dmt/internal/logger"
 	"github.com/deckhouse/dmt/internal/manager"
 	"github.com/deckhouse/dmt/pkg/config"
 )
 
-const (
-	numThreads = 10
-)
-
 func main() {
-	var (
-		printHelp    bool
-		printVersion bool
-		version      = "HEAD"
-	)
-
 	logger.InitLogger()
 
-	defaults := pflag.NewFlagSet("defaults for all commands", pflag.ExitOnError)
-	defaults.BoolVarP(&printHelp, "help", "h", false, "help message")
-	defaults.BoolVarP(&printVersion, "version", "v", false, "version message")
+	defaults := flags.InitDefaultFlagSet()
 
-	defaults.Usage = func() {
-		_, _ = fmt.Fprintln(os.Stderr, "Usage: dmt [gen|lint] [OPTIONS]")
-		defaults.PrintDefaults()
-	}
-
-	lint := pflag.NewFlagSet("lint", pflag.ContinueOnError)
+	lint := flags.InitLintFlagSet()
 	lint.AddFlagSet(defaults)
 
-	lint.IntVarP(&flags.LintersLimit, "parallel", "p", numThreads, "number of threads for parallel processing")
-	lint.StringVarP(&flags.LogLevel, "log-level", "l", "INFO", "log-level [DEBUG | INFO | WARN | ERROR]")
-
-	lint.Usage = func() {
-		_, _ = fmt.Fprintln(os.Stderr, "Usage: dmt lint [OPTIONS] [dirs...]")
-		lint.PrintDefaults()
-	}
-
-	gen := pflag.NewFlagSet("gen", pflag.ContinueOnError)
+	gen := flags.InitGenFlagSet()
 	gen.AddFlagSet(defaults)
-
-	gen.Usage = func() {
-		_, _ = fmt.Fprintln(os.Stderr, "Usage: dmt gen [OPTIONS]")
-		defaults.PrintDefaults()
-	}
 
 	if len(os.Args) < 2 {
 		defaults.Usage()
@@ -60,20 +28,7 @@ func main() {
 
 	switch os.Args[1] {
 	case "lint":
-		if err := lint.Parse(os.Args[2:]); err != nil {
-			lint.Usage()
-			return
-		}
-
-		if printHelp {
-			lint.Usage()
-			return
-		}
-
-		if printVersion {
-			fmt.Println("dmt version: ", version)
-			return
-		}
+		flags.GeneralParse(lint)
 
 		var dirs = lint.Args()
 		if len(dirs) == 0 {
@@ -84,47 +39,28 @@ func main() {
 			return
 		}
 
-		logger.InfoF("Dirs: %v", dirs)
-
-		cfg, err := config.NewDefault(dirs)
-		logger.CheckErr(err)
-
-		mng := manager.NewManager(dirs, cfg)
-		result := mng.Run()
-		if result.ConvertToError() != nil {
-			fmt.Printf("%s\n", result.ConvertToError())
-		}
-
-		if result.Critical() {
-			os.Exit(1)
-		}
+		runLint(dirs)
 	case "gen":
-		if err := gen.Parse(os.Args[2:]); err != nil {
-			gen.Usage()
-			return
-		}
-
-		if printVersion {
-			fmt.Println("dmt version: ", version)
-			return
-		}
-
-		if printHelp {
-			gen.Usage()
-			return
-		}
+		flags.GeneralParse(gen)
 	default:
-		if err := defaults.Parse(os.Args[1:]); err != nil {
-			defaults.Usage()
-			return
-		}
-
-		if printVersion {
-			fmt.Println("dmt version: ", version)
-			return
-		}
-
+		flags.GeneralParse(defaults)
 		defaults.Usage()
-		return
+	}
+}
+
+func runLint(dirs []string) {
+	logger.InfoF("Dirs: %v", dirs)
+
+	cfg, err := config.NewDefault(dirs)
+	logger.CheckErr(err)
+
+	mng := manager.NewManager(dirs, cfg)
+	result := mng.Run()
+	if result.ConvertToError() != nil {
+		fmt.Printf("%s\n", result.ConvertToError())
+	}
+
+	if result.Critical() {
+		os.Exit(1)
 	}
 }
--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -1,6 +1,78 @@
 package flags
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/pflag"
+)
+
+const (
+	numThreads = 10
+)
+
 var (
 	LintersLimit int
 	LogLevel     string
 )
+
+var (
+	PrintHelp    bool
+	PrintVersion bool
+	Version      = "HEAD"
+)
+
+func InitDefaultFlagSet() *pflag.FlagSet {
+	defaults := pflag.NewFlagSet("defaults for all commands", pflag.ExitOnError)
+	defaults.BoolVarP(&PrintHelp, "help", "h", false, "help message")
+	defaults.BoolVarP(&PrintVersion, "version", "v", false, "version message")
+
+	defaults.Usage = func() {
+		_, _ = fmt.Fprintln(os.Stderr, "Usage: dmt [gen|lint] [OPTIONS]")
+		defaults.PrintDefaults()
+	}
+
+	return defaults
+}
+
+func InitLintFlagSet() *pflag.FlagSet {
+	lint := pflag.NewFlagSet("lint", pflag.ContinueOnError)
+
+	lint.IntVarP(&LintersLimit, "parallel", "p", numThreads, "number of threads for parallel processing")
+	lint.StringVarP(&LogLevel, "log-level", "l", "INFO", "log-level [DEBUG | INFO | WARN | ERROR]")
+
+	lint.Usage = func() {
+		_, _ = fmt.Fprintln(os.Stderr, "Usage: dmt lint [OPTIONS] [dirs...]")
+		lint.PrintDefaults()
+	}
+
+	return lint
+}
+
+func InitGenFlagSet() *pflag.FlagSet {
+	gen := pflag.NewFlagSet("gen", pflag.ContinueOnError)
+
+	gen.Usage = func() {
+		_, _ = fmt.Fprintln(os.Stderr, "Usage: dmt gen [OPTIONS]")
+		pflag.PrintDefaults()
+	}
+
+	return gen
+}
+
+func GeneralParse(flagSet *pflag.FlagSet) {
+	if err := flagSet.Parse(os.Args[2:]); err != nil {
+		flagSet.Usage()
+		os.Exit(0)
+	}
+
+	if PrintHelp {
+		flagSet.Usage()
+		os.Exit(0)
+	}
+
+	if PrintVersion {
+		fmt.Println("dmt version: ", Version)
+		os.Exit(0)
+	}
+}
