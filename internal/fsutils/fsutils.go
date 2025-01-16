2005d0b14e5441582ac5a0ad22218e382e8e782b
Evsyukov Denis <denis.evsyukov@flant.com>
2025-01-16 10:28:40 +0300

    feat: add IsFile function to check if a path is a file

--- internal/fsutils/fsutils.go
+++ internal/fsutils/fsutils.go
@@ -7,9 +7,17 @@ import (
 	"sync"
 )
 
-func IsDir(filename string) bool {
-	fi, err := os.Stat(filename)
+// IsDir checks if the given path is a directory
+func IsDir(path string) bool {
+	fi, err := os.Stat(path)
 	return err == nil && fi.IsDir()
+}
+
+// IsFile checks if the given path is a file
+func IsFile(path string) bool {
+	info, err := os.Stat(path)
+
+	return err == nil && !info.IsDir()
 }
 
 func ShortestRelPath(path, wd string) (string, error) {
