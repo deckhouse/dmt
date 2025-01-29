6b5329867febbb48882c6d02446f66b7471a37a6
Evsyukov Denis <denis.evsyukov@flant.com>
2025-01-29 10:24:23 +0300

    refactor: return new instance of LintRuleErrorsList in WithObjectID and WithModuleID methods

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -49,15 +49,21 @@ func NewLinterRuleList(linterID string, module ...string) *LintRuleErrorsList {
 }
 
 func (l *LintRuleErrorsList) WithObjectID(objectID string) *LintRuleErrorsList {
-	l.objectID = objectID
-
-	return l
+	return &LintRuleErrorsList{
+		storage:  l.storage,
+		linterID: l.linterID,
+		moduleID: l.moduleID,
+		objectID: objectID,
+	}
 }
 
 func (l *LintRuleErrorsList) WithModuleID(moduleID string) *LintRuleErrorsList {
-	l.moduleID = moduleID
-
-	return l
+	return &LintRuleErrorsList{
+		storage:  l.storage,
+		linterID: l.linterID,
+		moduleID: moduleID,
+		objectID: l.objectID,
+	}
 }
 
 func (l *LintRuleErrorsList) AddValue(value any, template string, a ...any) *LintRuleErrorsList {
