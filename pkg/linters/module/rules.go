50c51202eb4d9385c89297e515c6e727fa963673
Evsyukov Denis <denis.evsyukov@flant.com>
2025-02-03 15:22:09 +0300

    feat: add warning handling for file access and parsing errors in module YAML checks

--- pkg/linters/module/rules.go
+++ pkg/linters/module/rules.go
@@ -39,41 +39,46 @@ type ModulePlatformRequirements struct {
 
 func checkModuleYaml(moduleName, modulePath string) *errors.LintRuleErrorsList {
 	result := errors.NewLinterRuleList(ID, moduleName).WithObjectID(moduleName)
-	if slices.Contains(Cfg.SkipCheckModuleYaml, moduleName) {
-		return nil
-	}
 
 	_, err := os.Stat(filepath.Join(modulePath, ModuleConfigFilename))
 	if errs.Is(err, os.ErrNotExist) {
 		return nil
 	}
 	if err != nil {
-		return result.Add(
-			"Cannot stat file %q: %s",
-			ModuleConfigFilename, err.Error(),
-		)
+		return result.
+			WithWarning(slices.Contains(Cfg.SkipCheckModuleYaml, moduleName)).
+			Add(
+				"Cannot stat file %q: %s",
+				ModuleConfigFilename, err.Error(),
+			)
 	}
 
 	yamlFile, err := os.ReadFile(filepath.Join(modulePath, ModuleConfigFilename))
 	if err != nil {
-		return result.Add(
-			"Cannot read file %q: %s",
-			ModuleConfigFilename, err.Error(),
-		)
+		return result.
+			WithWarning(slices.Contains(Cfg.SkipCheckModuleYaml, moduleName)).
+			Add(
+				"Cannot read file %q: %s",
+				ModuleConfigFilename, err.Error(),
+			)
 	}
 
 	var yml DeckhouseModule
 
 	err = yaml.Unmarshal(yamlFile, &yml)
 	if err != nil {
-		return result.Add(
-			"Cannot parse file %q: %s",
-			ModuleConfigFilename, err.Error(),
-		)
+		return result.
+			WithWarning(slices.Contains(Cfg.SkipCheckModuleYaml, moduleName)).
+			Add(
+				"Cannot parse file %q: %s",
+				ModuleConfigFilename, err.Error(),
+			)
 	}
 
 	if yml.Name == "" {
-		result.Add("Field %q is required", "name")
+		result.
+			WithWarning(slices.Contains(Cfg.SkipCheckModuleYaml, moduleName)).
+			Add("Field %q is required", "name")
 	}
 
 	stages := []string{
@@ -84,10 +89,12 @@ func checkModuleYaml(moduleName, modulePath string) *errors.LintRuleErrorsList {
 	}
 
 	if yml.Stage != "" && !slices.Contains(stages, yml.Stage) {
-		result.Add(
-			"Field %q is not one of the following values: %q",
-			"stage", strings.Join(stages, ", "),
-		)
+		result.
+			WithWarning(slices.Contains(Cfg.SkipCheckModuleYaml, moduleName)).
+			Add(
+				"Field %q is not one of the following values: %q",
+				"stage", strings.Join(stages, ", "),
+			)
 	}
 
 	if yml.Requirements != nil {
@@ -101,28 +108,34 @@ func (m ModuleRequirements) validateRequirements(moduleName string) *errors.Lint
 	result := errors.NewLinterRuleList(ID, moduleName).WithObjectID(moduleName)
 	if m.Deckhouse != "" {
 		if _, err := semver.NewConstraint(m.Deckhouse); err != nil {
-			result.Add(
-				"Invalid Deckhouse version requirement: %s",
-				err.Error(),
-			)
+			result.
+				WithWarning(slices.Contains(Cfg.SkipCheckModuleYaml, moduleName)).
+				Add(
+					"Invalid Deckhouse version requirement: %s",
+					err.Error(),
+				)
 		}
 	}
 
 	if m.Kubernetes != "" {
 		if _, err := semver.NewConstraint(m.Kubernetes); err != nil {
-			result.Add(
-				"Invalid Kubernetes version requirement: %s",
-				err.Error(),
-			)
+			result.
+				WithWarning(slices.Contains(Cfg.SkipCheckModuleYaml, moduleName)).
+				Add(
+					"Invalid Kubernetes version requirement: %s",
+					err.Error(),
+				)
 		}
 	}
 
 	for parentModuleName, parentModuleVersion := range m.ParentModules {
 		if _, err := semver.NewConstraint(parentModuleVersion); err != nil {
-			result.Add(
-				"Invalid module %q version requirement: %s",
-				parentModuleName, err.Error(),
-			)
+			result.
+				WithWarning(slices.Contains(Cfg.SkipCheckModuleYaml, moduleName)).
+				Add(
+					"Invalid module %q version requirement: %s",
+					parentModuleName, err.Error(),
+				)
 		}
 	}
 
