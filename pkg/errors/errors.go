b768c3752317522ae1c560a79f7d7f27475718a6
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2025-02-06 21:58:21 +0300

    bump comments
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -204,7 +204,7 @@ func (l *LintRuleErrorsList) Errorf(template string, a ...any) *LintRuleErrorsLi
 	return l.add(fmt.Sprintf(template, a...), pkg.Error)
 }
 
-// Deprecated: use Critical or Criticalf instead
+// Deprecated: use Errir or Errorf instead
 func (l *LintRuleErrorsList) Add(templateOrString string, a ...any) *LintRuleErrorsList {
 	if len(a) != 0 {
 		templateOrString = fmt.Sprintf(templateOrString, a...)
