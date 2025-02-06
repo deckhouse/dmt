8d233c6122e8dc59c1e50410b03d912372cb4ad8
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2025-02-06 16:58:20 +0300

    draft
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -13,15 +13,24 @@ type ModuleConfig struct {
 	LintersSettings LintersSettings `mapstructure:"linters-settings"`
 }
 
-func (cfg *LintersSettings) MergeGlobal(gcfg *global.Global) {
-	cfg.OpenAPI.Impact = gcfg.Linters.Openapi.Impact
-	cfg.NoCyrillic.Impact = gcfg.Linters.NoCyrillic.Impact
-	cfg.License.Impact = gcfg.Linters.License.Impact
-	cfg.OSS.Impact = gcfg.Linters.OSS.Impact
-	cfg.Probes.Impact = gcfg.Linters.Probes.Impact
-	cfg.Container.Impact = gcfg.Linters.Container.Impact
-	cfg.K8SResources.Impact = gcfg
-	cfg.CRDResources.Impact = gcfg.Linters.CRD.Impact
+func (cfg *LintersSettings) MergeGlobal(lcfg *global.Linters) {
+	cfg.OpenAPI.Impact = lcfg.OpenAPI.Impact
+	cfg.NoCyrillic.Impact = lcfg.NoCyrillic.Impact
+	cfg.License.Impact = lcfg.License.Impact
+	cfg.OSS.Impact = lcfg.OSS.Impact
+	cfg.Probes.Impact = lcfg.Probes.Impact
+	cfg.Container.Impact = lcfg.Container.Impact
+	cfg.K8SResources.Impact = lcfg.K8SResources.Impact
+	cfg.VPAResources.Impact = lcfg.VPAResources.Impact
+	cfg.PDBResources.Impact = lcfg.PDBResources.Impact
+	cfg.CRDResources.Impact = lcfg.CRDResources.Impact
+	cfg.Images.Impact = lcfg.Images.Impact
+	cfg.Rbac.Impact = lcfg.Rbac.Impact
+	cfg.Resources.Impact = lcfg.Resources.Impact
+	cfg.Monitoring.Impact = lcfg.Monitoring.Impact
+	cfg.Ingress.Impact = lcfg.Ingress.Impact
+	cfg.Module.Impact = lcfg.Module.Impact
+	cfg.Conversions.Impact = lcfg.Conversions.Impact
 }
 
 func NewDefaultRootConfig(dirs []string) (*RootConfig, error) {
