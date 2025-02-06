989b78998346ec3bb7c1892af58f0ff64e2a7750
Pavel Okhlopkov <pavel.okhlopkov@flant.com>
2025-02-06 16:52:31 +0300

    fix linter names
    Signed-off-by: Pavel Okhlopkov <pavel.okhlopkov@flant.com>

--- pkg/config/global/global.go
+++ pkg/config/global/global.go
@@ -21,22 +21,23 @@ type Global struct {
 }
 
 type Linters struct {
-	Openapi     LinterConfig `mapstructure:"openapi"`
-	NoCyrillic  LinterConfig `mapstructure:"nocyrillic"`
-	License     LinterConfig `mapstructure:"license"`
-	OSS         LinterConfig `mapstructure:"oss"`
-	Probes      LinterConfig `mapstructure:"probes"`
-	Container   LinterConfig `mapstructure:"container"`
-	RBACProxy   LinterConfig `mapstructure:"rbacproxy"`
-	VPA         LinterConfig `mapstructure:"vpa"`
-	PDB         LinterConfig `mapstructure:"pdb"`
-	CRD         LinterConfig `mapstructure:"crd"`
-	Images      LinterConfig `mapstructure:"images"`
-	RBAC        LinterConfig `mapstructure:"rbac"`
-	Monitoring  LinterConfig `mapstructure:"monitoring"`
-	Ingress     LinterConfig `mapstructure:"ingress"`
-	Module      LinterConfig `mapstructure:"module"`
-	Conversions LinterConfig `mapstructure:"conversions"`
+	OpenAPI      LinterConfig `mapstructure:"openapi"`
+	NoCyrillic   LinterConfig `mapstructure:"nocyrillic"`
+	License      LinterConfig `mapstructure:"license"`
+	OSS          LinterConfig `mapstructure:"oss"`
+	Probes       LinterConfig `mapstructure:"probes"`
+	Container    LinterConfig `mapstructure:"container"`
+	K8SResources LinterConfig `mapstructure:"k8s_resources"`
+	VPAResources LinterConfig `mapstructure:"vpa_resources"`
+	PDBResources LinterConfig `mapstructure:"pdb_resources"`
+	CRDResources LinterConfig `mapstructure:"crd_resources"`
+	Images       LinterConfig `mapstructure:"images"`
+	Rbac         LinterConfig `mapstructure:"rbac"`
+	Resources    LinterConfig `mapstructure:"resources"`
+	Monitoring   LinterConfig `mapstructure:"monitoring"`
+	Ingress      LinterConfig `mapstructure:"ingress"`
+	Module       LinterConfig `mapstructure:"module"`
+	Conversions  LinterConfig `mapstructure:"conversions"`
 }
 
 type LinterConfig struct {
