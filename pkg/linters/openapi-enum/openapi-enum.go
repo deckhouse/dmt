4c4b001f79fb92fcf0693115c4209ce3658bd301
Evsyukov Denis <denis.evsyukov@flant.com>
2025-02-07 14:01:59 +0300

    wip

--- pkg/linters/openapi-enum/openapi-enum.go
+++ pkg/linters/openapi-enum/openapi-enum.go
@@ -16,36 +16,37 @@ import (
 type Enum struct {
 	name, desc string
 	cfg        *config.OpenAPIEnumSettings
+	ErrorList  *errors.LintRuleErrorsList
 }
 
-func New(cfg *config.OpenAPIEnumSettings) *Enum {
+func New(cfg *config.ModuleConfig, errorList *errors.LintRuleErrorsList) *Enum {
 	return &Enum{
-		name: "openapi-enum",
-		desc: "Linter will check openapi enum values is correct",
-		cfg:  cfg,
+		name:      "openapi-enum",
+		desc:      "Linter will check openapi enum values is correct",
+		cfg:       &cfg.LintersSettings.OpenAPIEnum,
+		ErrorList: errorList.WithLinterID("openapi-enum").WithMaxLevel(cfg.LintersSettings.Conversions.Impact),
 	}
 }
 
-func (o *Enum) Run(m *module.Module) *errors.LintRuleErrorsList {
-	result := errors.NewLinterRuleList("openapi-enum", m.GetName())
-
+func (e *Enum) Run(m *module.Module) *errors.LintRuleErrorsList {
+	errorLists := e.ErrorList.WithModule(m.GetName())
 	files := fsutils.GetFiles(m.GetPath(), true, filterFiles)
-	parser := NewEnumValidator(o.cfg)
+	parser := NewEnumValidator(e.cfg)
 	for _, file := range files {
 		if err := openapi.Parse(parser, file); err != nil {
-			result.WithValue(err).Add("openAPI file is not valid: %s", fsutils.Rel(m.GetPath(), file))
+			errorLists.WithValue(err).Errorf("openAPI file is not valid: %s", fsutils.Rel(m.GetPath(), file))
 		}
 	}
 
-	return result
+	return errorLists
 }
 
-func (o *Enum) Name() string {
-	return o.name
+func (e *Enum) Name() string {
+	return e.name
 }
 
-func (o *Enum) Desc() string {
-	return o.desc
+func (e *Enum) Desc() string {
+	return e.desc
 }
 
 var r = regexp.MustCompile(`.*(?:openapi|crds)/.*\.ya?ml$`)
