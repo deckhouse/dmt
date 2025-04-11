ee99efb4c73c220ef7f2a9e863c9f08e7e9fc059
Evsyukov Denis <denis.evsyukov@flant.com>
2025-04-11 10:34:47 +0300

    refactor: remove unused ShortestRelPath function from fsutils.go
    Signed-off-by: Evsyukov Denis <denis.evsyukov@flant.com>

--- internal/fsutils/fsutils.go
+++ internal/fsutils/fsutils.go
@@ -38,41 +38,6 @@ func IsFile(path string) bool {
 	return err == nil && !info.IsDir()
 }
 
-// ShortestRelPath returns the shortest relative path from the working directory to the given path.
-func ShortestRelPath(path, wd string) (string, error) {
-	if wd == "" { // get it if user don't have cached working dir
-		var err error
-		wd, err = Getwd()
-		if err != nil {
-			return "", fmt.Errorf("can't get working directory: %w", err)
-		}
-	}
-
-	evaledPath, err := EvalSymlinks(path)
-	if err != nil {
-		return "", fmt.Errorf("can't eval symlinks for path %s: %w", path, err)
-	}
-	path = evaledPath
-
-	// make path absolute and then relative to be able to fix this case:
-	// we are in /test dir, we want to normalize ../test, and have file file.go in this dir;
-	// it must have normalized path file.go, not ../test/file.go,
-	var absPath string
-	if filepath.IsAbs(path) {
-		absPath = path
-	} else {
-		absPath = filepath.Join(wd, path)
-	}
-
-	relPath, err := filepath.Rel(wd, absPath)
-	if err != nil {
-		return "", fmt.Errorf("can't get relative path for path %s and root %s: %w",
-			absPath, wd, err)
-	}
-
-	return relPath, nil
-}
-
 var (
 	cachedWd      string
 	cachedWdError error
