3f1a6ea27271cf562c9fbba70f726f97a9ea017a
Evsyukov Denis <denis.evsyukov@flant.com>
2025-06-02 09:44:48 +0300

    fix: use require.Empty for asserting empty content in TestGet for base_images.yml

--- internal/werf/files_test.go
+++ internal/werf/files_test.go
@@ -99,7 +99,7 @@ func TestGet(t *testing.T) {
 	require.Equal(t, "test content", content, "file content not matches: expected 'test content', got '%s'", content)
 
 	content = f.Get("base_images.yml")
-	require.Equal(t, "", content, "file content not matches: expected '', got '%s'", content)
+	require.Empty(t, content, "file content not matches: expected '', got '%s'", content)
 
 	defer func() {
 		if r := recover(); r == nil {
