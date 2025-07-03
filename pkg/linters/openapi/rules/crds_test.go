c484ce9cd7ac50e6f7162e281520b86989a06f69
Evsyukov Denis <denis.evsyukov@flant.com>
2025-07-03 10:18:59 +0300

    feat: add validation for deprecated keys in CRD YAML files

--- pkg/linters/openapi/rules/crds_test.go
+++ pkg/linters/openapi/rules/crds_test.go
@@ -140,6 +140,66 @@ spec:
           type: object`,
 			wantErrors: nil,
 		},
+		{
+			name:       "CRD with deprecated key",
+			moduleName: "test-module",
+			content: `apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  name: test.deckhouse.io
+  labels:
+    heritage: deckhouse
+    module: test-module
+spec:
+  group: deckhouse.io
+  names:
+    kind: Test
+    plural: tests
+  scope: Cluster
+  versions:
+    - name: v1
+      served: true
+      storage: true
+      schema:
+        openAPIV3Schema:
+          type: object
+          properties:
+            deprecatedField:
+              type: string
+              deprecated: true
+              description: This field is deprecated`,
+			wantErrors: []string{`CRD contains deprecated key, use "x-doc-deprecated: true" instead`},
+		},
+		{
+			name:       "CRD with x-doc-deprecated key (should not error)",
+			moduleName: "test-module",
+			content: `apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  name: test.deckhouse.io
+  labels:
+    heritage: deckhouse
+    module: test-module
+spec:
+  group: deckhouse.io
+  names:
+    kind: Test
+    plural: tests
+  scope: Cluster
+  versions:
+    - name: v1
+      served: true
+      storage: true
+      schema:
+        openAPIV3Schema:
+          type: object
+          properties:
+            deprecatedField:
+              type: string
+              x-doc-deprecated: true
+              description: This field is deprecated`,
+			wantErrors: nil,
+		},
 	}
 
 	for _, tt := range tests {
