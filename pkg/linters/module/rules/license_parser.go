444df7addedf5f41d11f924263ff388e07810cdb
Michail Sinelnikov <72449391+RottenRat@users.noreply.github.com>
2025-10-08 11:30:19 +0300

    [dmt] Add readme examples and CE modified template (#285)
    Signed-off-by: Sinelnikov Michail <mikhail.sinelnikov@flant.com>
    Co-authored-by: Sinelnikov Michail <mikhail.sinelnikov@flant.com>

--- pkg/linters/module/rules/license_parser.go
+++ pkg/linters/module/rules/license_parser.go
@@ -84,6 +84,26 @@ limitations under the License.`,
 				YearPattern: `20[0-9]{2}`,
 			},
 			{
+				Type: "CE",
+				Name: "Apache License 2.0 Modified",
+				Template: `Copyright {{YEAR}} Flant JSC
+
+Modifications made by Flant JSC as part of the Deckhouse project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`,
+				YearPattern: `20[0-9]{2}`,
+			},
+			{
 				Type: "EE",
 				Name: "Deckhouse Platform Enterprise Edition",
 				Template: `Copyright {{YEAR}} Flant JSC
