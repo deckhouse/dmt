e6244c3d25a8e2d8003835f3bf473636e7952650
diyliv <onlogn081@gmail.com>
2026-04-15 16:05:10 +0300

    deleted useless fields
    Signed-off-by: diyliv <onlogn081@gmail.com>

--- pkg/testers/tester.go
+++ pkg/testers/tester.go
@@ -16,54 +16,13 @@ limitations under the License.
 
 package testers
 
-import (
-	"errors"
-	"fmt"
-)
-
-var ErrNotApplicable = errors.New("not applicable")
-
-type notApplicableError struct {
-	reason string
-}
-
-func (e *notApplicableError) Error() string {
-	return fmt.Sprintf("not applicable: %s", e.reason)
-}
-
-func (e *notApplicableError) Is(target error) bool {
-	return target == ErrNotApplicable
-}
-
-func NotApplicable(reason string) error {
-	return &notApplicableError{reason: reason}
-}
-
-type TestError struct {
-	TestName string
-	Message  string
-	Expected string
-	Got      string
-}
-
-func (e *TestError) Error() string {
-	if e.Expected != "" && e.Got != "" {
-		return fmt.Sprintf("%s: %s; expected: %s; got: %s", e.TestName, e.Message, e.Expected, e.Got)
-	}
-	return fmt.Sprintf("%s: %s", e.TestName, e.Message)
-}
-
-// Tester is the interface that all module testers must implement.
-// Each tester validates a specific aspect of a module (conversions, values, render, etc.)
+// Tester is the interface for test modules.
 type Tester interface {
-	// Run executes the test on the given module path.
-	// Returns nil if test passed, returns error describing the failure.
-	// Returns ErrNotApplicable if the test is not applicable to this module.
-	Run(modulePath string) error
+	// Run executes the tester against the given module path.
+	// Returns true if the tester was applicable to this module.
+	Run(modulePath string) bool
 
-	// Name returns the test name (e.g., "conversions", "values").
 	Name() string
 
-	// Desc returns a short description of what the test does.
 	Desc() string
 }
