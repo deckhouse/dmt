28ed14518d058a0fc9e870cab153f16043b3553f
Smyslov Maxim <maksim.smyslov@flant.com>
2026-06-11 17:44:33 +0300

    [dmt] Remove TestCheckTemplatesUsingRenderedImages
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- pkg/linters/templates/rules/werf_test.go
+++ pkg/linters/templates/rules/werf_test.go
@@ -7,10 +7,8 @@ import (
 
 	"github.com/gojuno/minimock/v3"
 	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	"github.com/deckhouse/dmt/internal/mocks"
-	"github.com/deckhouse/dmt/internal/storage"
 	"github.com/deckhouse/dmt/pkg/errors"
 )
 
@@ -31,15 +29,6 @@ func TestValidateWerfTemplates(t *testing.T) {
 	mock := mocks.NewModuleMock(mc)
 	mock.GetPathMock.Return("/mock/path")
 	mock.GetNameMock.Return("mock-module")
-	mock.GetStorageMock.Return(map[storage.ResourceIndex]storage.StoreObject{
-		{
-			Kind:      "Deployment",
-			Name:      "test-deployment",
-			Namespace: "test-namespace",
-		}: {
-			AbsPath: filePath,
-		},
-	})
 	mock.GetWerfFileMock.Return(`
 image: mock-module/test-image
 git:
@@ -66,16 +55,6 @@ git:
 # Missing stageDependencies
 
 `)
-	mockModuleWerfInvalid.GetStorageMock.Return(map[storage.ResourceIndex]storage.StoreObject{
-		{
-			Kind:      "Deployment",
-			Name:      "test-deployment",
-			Namespace: "test-namespace",
-		}: {
-			AbsPath: filePath,
-		},
-	})
-
 	rule.ValidateWerfTemplates(mockModuleWerfInvalid, errorList)
 	assert.True(t, errorList.ContainsErrors(), "Expected errors for invalid Werf file")
 	assert.Contains(t, errorList.GetErrors()[0].Text, "is missing required 'git.stageDependencies' field")
@@ -127,71 +106,6 @@ git:
 	checkGitSection("mock-module", malformedManifests, errorList)
 	assert.True(t, errorList.ContainsErrors(), "Expected errors for malformed YAML")
 	assert.Contains(t, errorList.GetErrors()[0].Text, "mock-module/test-image")
-}
-
-func TestCheckTemplatesUsingRenderedImages(t *testing.T) {
-	dir := t.TempDir()
-
-	filePath := filepath.Join(dir, "test-image")
-	if err := os.WriteFile(filePath, []byte(`image: {{ include "helm_lib_module_image" . "mock-module/test-image" }}`), 0644); err != nil {
-		t.Fatal(err)
-	}
-
-	errorList := errors.NewLintRuleErrorsList()
-
-	// Mock module with valid Werf file
-	mc := minimock.NewController(t)
-
-	mock := mocks.NewModuleMock(mc)
-	mock.GetStorageMock.Return(map[storage.ResourceIndex]storage.StoreObject{
-		{
-			Kind:      "Deployment",
-			Name:      "test-deployment",
-			Namespace: "test-namespace",
-		}: {
-			AbsPath: filePath,
-			Unstructured: unstructured.Unstructured{
-				Object: map[string]any{
-					"image": "mock-module/test-image",
-				},
-			},
-		},
-	})
-	mock.GetWerfFileMock.Return(`
-image: mock-module/test-image
-git:
-- add: /deckhouse/modules/910-test-module/images/test-image
-  to: /src
-  stageDependencies:
-    install:
-    - '**/*.sh'
-`)
-
-	for _, object := range mock.GetStorage() {
-		checkTemplatesUsingRenderedImages(object, []string{mock.GetWerfFile()}, errorList)
-	}
-
-	assert.False(t, errorList.ContainsErrors(), "Expected no errors for valid manifest")
-
-	// Invalid manifest
-	invalidManifests := []string{
-		`
-image: mock-module/test-image-invalid
-git:
-- add: /deckhouse/modules/910-test-module/images/test-image
-  to: /src
-  stageDependencies:
-    install:
-    - '**/*.sh'
-`,
-	}
-
-	for _, object := range mock.GetStorage() {
-		checkTemplatesUsingRenderedImages(object, invalidManifests, errorList)
-	}
-
-	assert.True(t, errorList.ContainsErrors(), "Expected errors for invalid manifest")
-	assert.Contains(t, errorList.GetErrors()[0].Text, `Template references image "mock-module/test-image" which is not defined in werf.yaml`)
 }
 
 func TestCheckUnderscoredImages(t *testing.T) {
