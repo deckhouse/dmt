77f61036f044c228b9e8697b54b5e5cd8215107b
Ruslan Gorbunov <ruslan.gorbunov@flant.com>
2026-06-24 05:30:44 +0300

    add markdownlint rule to documentation linter
    Signed-off-by: Ruslan Gorbunov <ruslan.gorbunov@flant.com>

--- internal/module/module.go
+++ internal/module/module.go
@@ -393,7 +393,7 @@ func mapExclusionRulesAndSettings(linterSettings *pkg.LintersSettings, configSet
 	mapRBACExclusions(linterSettings, configSettings)
 	mapHooksSettings(linterSettings, configSettings)
 	mapModuleExclusionsAndSettings(linterSettings, configSettings)
-	mapDocumentationSettings(linterSettings, configSettings)
+	// no excluded rules - mapDocumentationExclusionsAndSettings(linterSettings, configSettings)
 }
 
 // mapContainerExclusions maps Container linter exclusion rules
@@ -501,10 +501,6 @@ func mapModuleExclusionsAndSettings(linterSettings *pkg.LintersSettings, configS
 	linterSettings.Module.DefinitionFileRuleSettings.Disable = configSettings.Module.DefinitionFile.Disable
 	linterSettings.Module.ConversionsRuleSettings.Disable = configSettings.Module.Conversions.Disable
 	linterSettings.Module.HelmignoreRuleSettings.Disable = configSettings.Module.Helmignore.Disable
-}
-
-// mapDocumentationSettings maps Documentation linter settings (placeholder for future exclusion rules)
-func mapDocumentationSettings(_ *pkg.LintersSettings, _ *config.LintersSettings) {
 }
 
 func NewModule(path string, vals *chartutil.Values, globalSchema *spec.Schema, rootConfig *config.RootConfig, errorList *dmtErrors.LintRuleErrorsList) (*Module, error) {
