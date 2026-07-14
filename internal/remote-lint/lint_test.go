19004ab171f52bdf16a7897278f825207e541ac2
Smyslov Maxim <maksim.smyslov@flant.com>
2026-07-14 14:06:05 +0300

    add test
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- internal/remote-lint/lint_test.go
+++ internal/remote-lint/lint_test.go
@@ -2,7 +2,23 @@ package remotelint
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestRunRemoteLint(t *testing.T) {
+func TestCutTagFromImagePath(t *testing.T) {
+	repository, tag, err := cutTagFromImagePath("registry.example.com/deckhouse/my-module:v0.0.1")
+	require.NoError(t, err)
+	require.Equal(t, "registry.example.com/deckhouse/my-module", repository)
+	require.Equal(t, "v0.0.1", tag)
+
+	repository, tag, err = cutTagFromImagePath("registry.example.com/deckhouse/my-module@sha256:1234567890")
+	require.NoError(t, err)
+	require.Equal(t, "registry.example.com/deckhouse/my-module", repository)
+	require.Equal(t, "sha256:1234567890", tag)
+
+	repository, tag, err = cutTagFromImagePath("registry.example.com/deckhouse/my-module")
+	require.Contains(t, err.Error(), "tag not found in image path")
+	require.Empty(t, repository)
+	require.Empty(t, tag)
 }
